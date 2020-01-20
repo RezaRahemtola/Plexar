e351d191fcd628c391866258189893a8f96bad8c
RezaRahemtola <rahemtola.reza@gmail.com>
2020-01-20 20:32:59 +0100

    Changed indentation size

--- TFC/client/main.js
+++ TFC/client/main.js
@@ -8,19 +8,19 @@ import '../imports/UI/body.js';
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+    // counter starts at 0
+    this.counter = new ReactiveVar(0);
 });
 
 Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+    counter() {
+        return Template.instance().counter.get();
+    },
 });
 
 Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+    'click button'(event, instance) {
+        // increment the counter when button is clicked
+        instance.counter.set(instance.counter.get() + 1);
+    },
 });
