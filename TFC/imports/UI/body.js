53564522212facd5bde37044e0778dc5b97e41d9
Evan Labourdette <evanlabourdette@MacBook-Pro-de-Evan.local>
2020-01-25 09:57:08 +0100

    Base de données 1

--- TFC/imports/UI/body.js
+++ TFC/imports/UI/body.js
@@ -4,3 +4,10 @@ import { Template } from 'meteor/templating';
 
 // HTML imports
 import './body.html';
+
+// Info indispensable pour les bases de données
+import { Meteor } from 'meteor/meteor'
+import { Template } from 'meteor/templating'
+
+//Imports pour les bases de données
+import { Annonces } from '../bdd/annonce.js'
