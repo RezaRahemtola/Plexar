2d026b96312bf5203a3361e4872d29a05f329375
RezaRahemtola <rahemtola.reza@gmail.com>
2020-01-26 16:30:05 +0100

    BIG UPDATE - custom login and register forms, logout button and users database started

--- TFC/imports/UI/body.js
+++ TFC/imports/UI/body.js
@@ -15,9 +15,10 @@ import { Produits } from '../bdd/produits.js';
 
 Session.set('page', 'home');
 
+
 Template.body.helpers({
     currentPage: function(page){
-    return Session.get('page');
+        return Session.get('page');
   }
 });
 
@@ -25,8 +26,8 @@ Template.body.events({
     'click #home' (event){
         Session.set('page', 'home');
     },
-    'click #utilisateur' (event){
-        Session.set('page', 'utilisateur');
+    'click #user' (event){
+        Session.set('page', 'user');
     },
     'click #manageProduit' (event){
         Session.set('page', 'manageProduit');
--- TFC/imports/UI/utilisateur.js
+++ TFC/imports/UI/utilisateur.js
@@ -4,3 +4,45 @@ import { Template } from 'meteor/templating';
 
 // HTML imports
 import './utilisateur.html';
+
+Template.register.events({
+    'submit form'(event){
+        event.preventDefault();
+        var form = new FormData(document.getElementById('register'));
+        var username = form.get('username');
+        var email = form.get('email');
+        var password = form.get('password');
+
+        // Creationg the new user
+        Accounts.createUser({
+            username: username,
+            email: email,
+            password: password
+        });
+        Session.set('page', 'user');
+    }
+});
+
+Template.login.events({
+    'submit form' (event){
+        event.preventDefault();
+        var form = new FormData(document.getElementById('login'));
+        var email = form.get('email');
+        var password = form.get('password');
+        Meteor.loginWithPassword(email, password);
+        Session.set('page', 'user');
+    }
+});
+
+Template.user.events({
+    'click .register' (event){
+        Session.set('page', 'register');
+    },
+    'click .logout' (event){
+        event.preventDefault();
+        Meteor.logout();
+    },
+    'click .login' (event){
+        Session.set('page', 'login');
+    }
+});
