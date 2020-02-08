e06eefefeeec6fc94c2b5bbc741b8e3c0bf68f9a
RezaRahemtola <rahemtola.reza@gmail.com>
2020-02-08 10:09:50 +0100

    Product page added

---
+++ TFC/imports/UI/productPage.js
@@ -0,0 +1,9 @@
+// Useful imports
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+// HTML imports
+import './productPage.html';
+
+// Database import
+import { Produits } from '../bdd/produits.js';
