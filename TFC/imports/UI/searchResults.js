854a45bc5040af71d774974a1df3856dbd6b92e1
RezaRahemtola <rahemtola.reza@gmail.com>
2020-03-08 12:41:38 +0100

    Search with text match in name and description working

--- TFC/imports/UI/searchResults.js
+++ TFC/imports/UI/searchResults.js
@@ -1,6 +1,7 @@
 // Useful imports
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { Mongo } from 'meteor/mongo';
 
 // Database imports
 import { Products } from '../bdd/products.js';
@@ -9,14 +10,31 @@ import { Shops } from '../bdd/shops.js';
 // HTML imports
 import './searchResults.html';
 
+console.log();
 
 Template.searchResults.helpers({
     displayProductsResults: function(){
-        var products = Products.find({name: Session.get("searchedText")});
-        return products
+        Meteor.call('searchForProducts', {text: Session.get("searchedText")}, function(error, result){
+            // Result is an array of products ID, we find the products in the db
+            var searchedProducts = []
+            for (var productID of result){
+                // For each product ID we add the product to the array
+                searchedProducts.push(Products.findOne({_id : productID}))
+            }
+            Session.set("searchedProducts", searchedProducts);  // Saving products in a Session variable
+        });
+        return Session.get("searchedProducts")
     },
     displayShopsResults: function(){
-        var shops = Shops.find({name: Session.get("searchedText")});
-        return shops
+        Meteor.call('searchForShops', {text: Session.get("searchedText")}, function(error, result){
+            // Result is an array of shops ID, we find the shops in the db
+            var searchedShops = []
+            for (var shopID of result){
+                // For each shop ID we add the shop to the array
+                searchedShops.push(Shops.findOne({_id : shopID}))
+            }
+            Session.set("searchedShops", searchedShops);  // Saving shops in a Session variable
+        });
+        return Session.get("searchedShops")
     }
 });
--- TFC/server/main.js
+++ TFC/server/main.js
@@ -6,12 +6,35 @@ import '../imports/bdd/usersInformations.js';
 import '../imports/bdd/favorites.js';
 import '../imports/bdd/shops.js';
 
+import { Products } from '../imports/bdd/products.js';
+import { Shops } from '../imports/bdd/shops.js';
+
 Meteor.startup(() => {
   // code to run on server at startup
+  Products.rawCollection().createIndex({ name: "text", description: "text" });
+  Shops.rawCollection().createIndex({ name: "text", description: "text" });
 });
 
 Meteor.methods({
     'changeUsername'({userId, newUsername}){
         Accounts.setUsername(userId, newUsername)
+    },
+    'searchForProducts'({text}){
+        var result = Products.find({$text: { $search: text}}).fetch();  // Return the matching products
+        var productsID = [];  // To save server resources we will only return products IDs
+        for (product of result){
+            // For each product we add its ID to the array
+            productsID.push(product._id);
+        }
+        return productsID  // return array of productsID
+    },
+    'searchForShops'({text}){
+        var result = Shops.find({$text: { $search: text}}).fetch();  // Return the matching products
+        var shopsID = [];  // To save server resources we will only return products IDs
+        for (shop of result){
+            // For each product we add its ID to the array
+            shopsID.push(shop._id);
+        }
+        return shopsID  // return array of productsID
     }
 });
